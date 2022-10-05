b8cf3b9ab92cf52be78a2e136ab2f38698dd0145
lillielove <lillielove@gmail.com>
2022-10-05 14:03:07 -0700

    rough pages structure as well as App.js router layout, all pages (in theory) created now

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,35 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    ApolloProvider, //special type of React component that we'll use to providate data to ALL of the other components
+    ApolloClient, //constructor function that will help initialize the connection to the GraphQL API server
+    // InMemoryCache, //enables AC instance to cache API response data so we can perform requests more efficiently
+    // createHttpLink, //allows us to control how the AC makes a request (like middleware for outbound network requests)
+} from "@apollo/client";
+
+import Header from "./Header";
+import { Create, Dashboard, Home, NoMatch } from "./pages";
+import Footer from "./Footer";
 
 function App() {
-    return <p>hello world hahahahahaha</p>;
+    return (
+        <Router>
+            <div className="flex-column justify-flex-start min-100-vh">
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/create" element={<Create />} />
+                        <Route path="*" element={<NoMatch />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
