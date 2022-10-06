0e7ecb5a9d9107445187c737aeab2fc693740357
lillielove <lillielove@gmail.com>
2022-10-05 18:10:18 -0700

    added Bootstrap CSS (we don't need to keep), rough basic info form down. Need to figure out address, how to import hours (using state?), and if separate parts of the info collection for the webpage should be moved to diff pages or not

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import TimePicker from "rc-time-picker";
+import "rc-time-picker/assets/index.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
 
 export default function Create() {
     // implement onBlur functionality here, so if someone skips the * required bits, we let them know it's BAD
+    const days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
+
+    const [formPage, setFormPage] = useState("basic");
+    const [time, setTime] = useState("");
+
+    const nextPage = (nextFormPage) => {
+        // check to see if there are any errors--if not, then go to the next page: basic, menu, additional
+    };
+
     return (
         <div>
             <h1>Let's start with some basic info.</h1>
@@ -14,7 +33,6 @@ export default function Create() {
                     <Form.Label>Restaurant Name*</Form.Label>
                     <Form.Control type="name" placeholder="Enter name" />
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Phone Number</Form.Label>
                     <Form.Control
@@ -22,28 +40,81 @@ export default function Create() {
                         placeholder="Enter phone number"
                     />
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Address</Form.Label>
                     <Form.Control type="address" placeholder="Enter address" />
                 </Form.Group>
 
-                <DropdownButton
-                    id="dropdown-basic-button"
-                    title="Dropdown button"
-                >
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">
-                        Another action
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
-                        Something else
-                    </Dropdown.Item>
-                </DropdownButton>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Day</th>
+                            <th>Opening</th>
+                            <th>Closing</th>
+                            <th>what to say here</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {days.map((day) => (
+                            <tr>
+                                <td>{day}</td>
+                                <td>
+                                    <TimePicker
+                                        placeholder="Select Time"
+                                        use12Hours
+                                        minuteStep={15}
+                                        showSecond={false}
+                                        focusOnOpen={true}
+                                        format="hh:mm A"
+                                        onChange={(e) =>
+                                            setTime(e.format("LT"))
+                                        }
+                                    />
+                                </td>
+                                <td>
+                                    <TimePicker
+                                        placeholder="Select Time"
+                                        use12Hours
+                                        minuteStep={15}
+                                        showSecond={false}
+                                        focusOnOpen={true}
+                                        format="hh:mm A"
+                                        onChange={(e) =>
+                                            setTime(e.format("LT"))
+                                        }
+                                    />
+                                </td>
+                                <td>
+                                    <Form>
+                                        <div
+                                            key={`default-checkbox`}
+                                            className="mb-3"
+                                        >
+                                            <Form.Check
+                                                type="checkbox"
+                                                id={`default-checkbox`}
+                                                label={"Closed all day"}
+                                            />
+                                        </div>
+                                    </Form>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
 
-                <Button variant="primary" type="submit">
+                <Button
+                    variant="primary"
+                    type="submit"
+                    // onClick={nextPage("menu") see if there are any errors, if not, then continue on to "menu" page}
+                >
                     Next
                 </Button>
+            </Form>
+
+                        <h1>What's on the menu?</h1>
+            <Form>
+                {/* plus button adds another form. */}
             </Form>
         </div>
     );
