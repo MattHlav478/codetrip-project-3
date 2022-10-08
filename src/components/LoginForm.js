8590705c2f8a6c939937a47b02d02ff389274338
marlowe <marlowepdx@gmail.com>
2022-10-08 09:43:19 -0700

    rewrote all changes which were overwritten

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Form, Button, Alert } from "react-bootstrap";
 import { auth } from "../services/firebaseConnection";
 import { connectAuthEmulator, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
-
-  const handleFormSubmit = async (event) => {
-    console.log("submit clicked");
-    event.preventDefault();
-    // clear form values
-    setUserFormData({
-      email: "",
-      password: "",
+    const [validated, setValidated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [userFormData, setUserFormData] = useState({
+        email: "",
+        password: "",
     });
+
+    const handleSignupBtn = (event) => {
+        window.location.assign("/signup")
+    }
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+        console.log(userFormData);
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log("submit clicked");
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            setShowAlert(true);
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            window.location.assign("/dashboard");
+            setValidated(true);
+            setShowAlert(false);
+        }
 
     // AUTHENTICATE USER SIGN-UP IN FIREBASE
     try {
@@ -35,55 +50,73 @@ const LoginForm = () => {
     } catch (error) {
       console.log(error);
     }
-  };
 
-  const handleSignupBtn = (event) => {
-    window.location.assign("/signup")
-}
+  };
 
 
   return (
     <>
-      <Form onSubmit={handleFormSubmit}>
-        <Form.Group>
-          <Form.Label htmlFor="email">Email</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your email"
-            name="email"
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-        </Form.Group>
+    <br></br>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+            <Alert
+                dismissible
+                onClose={() => setShowAlert(false)}
+                show={showAlert}
+                variant="danger"
+            >
+                Wrong login credentials!
+            </Alert>
+            <Form.Group>
+                <Form.Label htmlFor="email">Email</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Your email"
+                    name="email"
+                    onChange={handleInputChange}
+                    value={userFormData.email}
+                    required
+                />
+                <Form.Control.Feedback type="invalid">
+                    Email is required
+                </Form.Control.Feedback>
+            </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Your password"
-            name="password"
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-        </Form.Group>
+            <Form.Group>
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Your password"
+                    name="password"
+                    onChange={handleInputChange}
+                    value={userFormData.password}
+                    required
+                />
+                <Form.Control.Feedback type="invalid">
+                    Password is required!
+                </Form.Control.Feedback>
+            </Form.Group>
 
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type="submit"
-          variant="success"
-        >
-          Submit
+            <br></br>
+
+            <Button
+                disabled={!(userFormData.email && userFormData.password)}
+                type="submit"
+                variant="dark"
+            >
+                Submit
+            </Button>
+        </Form>
+        <br></br>
+        <h4>
+            Not a member?
+        </h4>
+        <Button 
+        onClick={handleSignupBtn}
+        variant="dark">
+           Sign up
         </Button>
-      </Form>
-      <br></br>
-      <h4>Not a member?</h4>
-      <Button onClick={handleSignupBtn} variant="dark">
-        Sign up
-      </Button>
     </>
-  );
+);
 };
 
 export default LoginForm;
