980bc883e03657082c122526be234bc7aae0c1fb
marlowe <marlowepdx@gmail.com>
2022-10-10 15:39:49 -0700

    updated meet the developers with photos, added a divider on home page, continued on never ending footer quest, applied containers to rogue elements, added css classes.

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NoMatch() {
     return (
-        <div>
+        <div className ="page-height">
             Oops, we couldn't find that page.
             <Link to="/">
                 <h1>Let's head back home, shall we?</h1>
