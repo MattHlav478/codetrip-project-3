8c439ac0b0c225e8bd6693d17c67e67dfc5b14de
lillielove <lillielove@gmail.com>
2022-10-09 18:58:08 -0700

    fixed signup form issue

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -10,114 +10,6 @@ import {
 } from "firebase/auth";
 
 const SignupForm = () => {
-<<<<<<< HEAD
-    //set initial form state
-    const [userFormData, setUserFormData] = useState({
-        name: "",
-        email: "",
-        password: "",
-    });
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserFormData({ ...userFormData, [name]: value });
-    };
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        console.log("submit clicked!");
-
-        // setUserFormData({
-        //   username: "",
-        //   email: "",
-        //   password: "",
-        // });
-
-        // exclude third arg to automatically generate unique id (for add doc)
-        // Add a new document with a generated id.
-        // const docRef = await addDoc(collection(db, "cities"), {
-        //   name: "Tokyo",
-        //   country: "Japan",
-        // });
-        // console.log("Document written with ID: ", docRef.id);
-
-        //STORE IN FIRESTORE
-        await setDoc(doc(db, "users", userFormData.email), {
-            name: userFormData.name,
-            password: userFormData.password,
-        });
-
-        // AUTHENTICATE USER SIGN-UP IN FIREBASE
-        try {
-            const userCredential = await createUserWithEmailAndPassword(
-                auth,
-                userFormData.email,
-                userFormData.password
-            );
-            console.log(userCredential.user);
-            window.location.assign("/");
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    return (
-        <>
-            {/* This is needed for the validation functionality above */}
-            <Form onSubmit={handleFormSubmit}>
-                <Form.Group>
-                    <Form.Label htmlFor="name">Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Your name"
-                        name="name"
-                        onChange={handleInputChange}
-                        value={userFormData.name}
-                        required
-                    />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label htmlFor="email">Email</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Your email address"
-                        name="email"
-                        onChange={handleInputChange}
-                        value={userFormData.email}
-                        required
-                    />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label htmlFor="password">Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Your password"
-                        name="password"
-                        onChange={handleInputChange}
-                        value={userFormData.password}
-                        required
-                    />
-                </Form.Group>
-                <Button
-                    disabled={
-                        !(
-                            userFormData.name &&
-                            userFormData.email &&
-                            userFormData.password
-                        )
-                    }
-                    type="submit"
-                    variant="success"
-                >
-                    Submit
-                </Button>
-            </Form>
-        </>
-    );
-};
-=======
   //set initial form state
   const [validated, setValidated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -233,6 +125,5 @@ const SignupForm = () => {
     </>
   );
 }
->>>>>>> develop
 
 export default SignupForm;
