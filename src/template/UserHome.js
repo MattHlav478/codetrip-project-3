33a1c34f3a806172533ff071cf5ef09fba0ad56b
kfisch2 <kmlindsey24@gmail.com>
2022-10-11 13:53:22 -0700

    Restaurant pages are rendering!!!!

--- src/template/UserHome.js
+++ src/template/UserHome.js
@@ -1,86 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
+import { db } from "../services/firebaseConnection";
+import { collection, query, where, getDoc, doc } from "firebase/firestore";
+import { UserAbout, UserFooter, UserHours, UserMenuItems } from "./index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// import {
-//     MDBNavbar,
-//     MDBNavbarNav,
-//     MDBNavbarItem,
-//     MDBNavbarLink,
-//     MDBNavbarToggler,
-//     MDBContainer,
-//     MDBIcon,
-//     MDBBtn,
-//     MDBCollapse,
-// } from "mdb-react-ui-kit";
+export default function Home() {
+  const { userId, name } = useParams();
+  console.log(userId, name);
+  // userParam is restaurant ID
 
-export default function Home({projects}) {
-    const [showBasic, setShowBasic] = useState(false);
+  async function getData() {
+    //     // FIRESTORE call
+    const docRef = doc(db, "restaurants", userId);
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data());
+    const x = docSnap.data();
+    console.log(x.name);
+  }
 
-    const { userId: userParam } = useParams();
-    console.log(userParam)
+  getData();
 
-    useEffect(() => {
-        console.log("projects: ", projects)
-    },[])
+  // console.log(docSnap.data())
 
-    // make a call to firebase to extract specific restaurant project data
-    // based on URL parameter bar
-    // genu/project/:user-id/:restaurant-id/:restaurant-name
+  // useEffect(() => {
+  //     console.log("projects: ", projects)
+  // },[])
 
-    return (
-        <header>
-            {/* <MDBNavbar expand="lg" light bgColor="white" fixed>
-                <MDBContainer fluid>
-                    <MDBNavbarToggler
-                        aria-controls="navbarExample01"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <MDBIcon fas icon="bars" />
-                    </MDBNavbarToggler>
-                    <MDBCollapse show={showBasic}>
-                        <MDBNavbarNav right className="mb-2 mb-lg-0">
-                            <MDBNavbarItem active>
-                                <MDBNavbarLink aria-current="page" href="#">
-                                    Home
-                                </MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink href="#">Features</MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink href="#">Pricing</MDBNavbarLink>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBNavbarLink href="#">About</MDBNavbarLink>
-                            </MDBNavbarItem>
-                        </MDBNavbarNav>
-                    </MDBCollapse>
-                </MDBContainer>
-            </MDBNavbar> */}
+  // make a call to firebase to extract specific restaurant project data
+  // based on URL parameter bar
+  // genu/project/:user-id/:restaurant-id/:restaurant-name
 
-            <div className="p-5 text-center bg-light">
-                <h1 className="mb-3">Restaurant Name</h1>
-                <h4 className="mb-3">323-499-5070</h4>
-                {/* <MDBBtn tag="a" outline size="lg">
-                    Our Menu
-                </MDBBtn> */}
-            </div>
+  return (
+    <header>
+      <div className="p-5 text-center bg-light">
+        <h1 className="mb-3">Restaurant Name</h1>
+        <h4 className="mb-3">323-499-5070</h4>
+      </div>
 
-            <Nav variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home">About Us</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Menu</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link>Visit Us</Nav.Link>
-                </Nav.Item>
-            </Nav>
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/">About Us</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href={`/restaurant/${userId}/${name}/menu`} eventKey="link-1">
+            Menu
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link >Visit Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
 
-            <br></br>
-        </header>
-    );
+      <br></br>
+    </header>
+  );
 }
