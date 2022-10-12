a89a45bf2e6ab61b992616358c8355134f37733e
lillielove <lillielove@gmail.com>
2022-10-12 13:31:39 -0700

    basicInfo changes

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "rc-time-picker/assets/index.css";
 
 export default function TableRow({ day, hours, setHours }) {
+    const [isClosed, setIsClosed] = useState(false);
+
     const handleChangeOpen = (value) => {
         const updateHours = { ...hours };
         updateHours[day] = { ...updateHours[day], open: value };
         setHours(updateHours);
+        console.log(hours[day]);
     };
     const handleChangeClose = (value) => {
         const updateHours = { ...hours };
         updateHours[day] = { ...updateHours[day], close: value };
         setHours(updateHours);
+        console.log(hours[day]);
     };
     const handleChangeCheckbox = (value) => {
         const updateHours = { ...hours };
@@ -23,27 +27,36 @@ export default function TableRow({ day, hours, setHours }) {
             <tr>
                 <td>{day}</td>
                 <td>
-                    <input
-                        type="time"
-                        className="start-time"
-                        name="open"
-                        onChange={(e) => handleChangeOpen(e.target.value)}
-                    />
+                    {!isClosed && (
+                        <input
+                            type="time"
+                            className="start-time"
+                            name="open"
+                            required
+                            onChange={(e) => handleChangeOpen(e.target.value)}
+                        />
+                    )}
                 </td>
                 <td>
-                    <input
-                        type="time"
-                        className="close-time"
-                        name="close"
-                        onChange={(e) => handleChangeClose(e.target.value)}
-                    />
+                    {!isClosed && (
+                        <input
+                            type="time"
+                            className="close-time"
+                            name="close"
+                            required
+                            onChange={(e) => handleChangeClose(e.target.value)}
+                        />
+                    )}
                 </td>
                 <td>
                     <input
                         id="isClosedCheck"
                         type="checkbox"
                         name="isClosed"
-                        onChange={(e) => handleChangeCheckbox(e.target.checked)}
+                        onChange={(e) => {
+                            handleChangeCheckbox(e.target.checked);
+                            setIsClosed(e.target.checked);
+                        }}
                     />
                     Closed All Day
                 </td>
