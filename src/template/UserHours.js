22ef8af94ae073e8d9e0ef3922bb5a9ad6e5cce1
lillielove <lillielove@gmail.com>
2022-10-12 14:44:13 -0700

    hours template page done

--- src/template/UserHours.js
+++ src/template/UserHours.js
@@ -1,21 +1,72 @@
-import UserNav from './UserNav'
+import UserNav from "./UserNav";
 import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
+import { useParams } from "react-router-dom";
+import { db } from "../services/firebaseConnection";
+import { getDoc, doc } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { isCompositeType } from "graphql";
 
-function FlushExample() {
+function UserHours() {
+    const { userId, name } = useParams();
+    const [hours, setHours] = useState();
+    const days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
+
+    function formatTimeShow(h_24) {
+        let amPm = "AM";
+        if (Number(h_24.substring(0, 2)) % 12 >= 0) {
+            amPm = "PM";
+        }
+        var h = Number(h_24.substring(0, 2)) % 12;
+        if (h === 0) h = 12;
+        return (h < 10 ? "0" : "") + h + ":00" + amPm;
+    }
+
+    async function getData() {
+        //     // FIRESTORE call
+        const docRef = doc(db, "restaurants", userId);
+        const docSnap = await getDoc(docRef);
+        const allHours = docSnap.data().hours;
+        const correctHours = days.map((day) => {
+            if (allHours[day].open || allHours[day].close) {
+                allHours[day].open = formatTimeShow(allHours[day].open);
+                allHours[day].close = formatTimeShow(allHours[day].close);
+            }
+            allHours[day].day = day; // adds the day as a property, alongside isClosed and open, close
+            return allHours[day];
+        });
+        setHours(correctHours);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <>
-            <UserNav/>
+            <UserNav />
             <ListGroup variant="flush">
-                <ListGroup.Item variant="danger">Mondays Closed</ListGroup.Item>
-                <ListGroup.Item>Tuesdays 10am-6pm</ListGroup.Item>
-                <ListGroup.Item variant="danger">
-                    Wednesdays Closed
-                </ListGroup.Item>
-                <ListGroup.Item>Thursdays 10am-6pm</ListGroup.Item>
-                <ListGroup.Item>Fridays 10am-6pm</ListGroup.Item>
-                <ListGroup.Item>Saturdays 10am-6pm</ListGroup.Item>
-                <ListGroup.Item variant="danger">Sundays Closed</ListGroup.Item>
+                {hours &&
+                    hours.map((everyDay) =>
+                        everyDay.isClosed ? (
+                            <ListGroup.Item variant="success">
+                                {everyDay.day} Closed
+                            </ListGroup.Item>
+                        ) : (
+                            <ListGroup.Item variant="success">
+                                {everyDay.day} {everyDay.open} -{" "}
+                                {everyDay.close}
+                            </ListGroup.Item>
+                        )
+                    )}
             </ListGroup>
 
             <br></br>
@@ -27,4 +78,4 @@ function FlushExample() {
     );
 }
 
-export default FlushExample;
+export default UserHours;
