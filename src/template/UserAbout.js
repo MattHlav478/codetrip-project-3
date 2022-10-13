657a0cb5369faede49a79fa8bc0c1f9a003a2c3d
lillielove <lillielove@gmail.com>
2022-10-12 17:05:44 -0700

    changes to BasicInfo

--- src/template/UserAbout.js
+++ src/template/UserAbout.js
@@ -1,33 +1,56 @@
 import UserNav from "./UserNav";
 import Card from "react-bootstrap/Card";
+import React, { useState, useEffect } from "react";
+
+import { useParams } from "react-router-dom";
+import { db } from "../services/firebaseConnection";
+import { getDoc, doc } from "firebase/firestore";
 
 function About() {
+    const { userId, name } = useParams();
+    const [about, setAbout] = useState([]);
+
+    async function getData() {
+        //     // FIRESTORE call
+        const docRef = doc(db, "restaurants", userId);
+        const docSnap = await getDoc(docRef);
+        const data = docSnap.data();
+        setAbout(data.about);
+        // const menuData = [];
+        // restData.menu.map((menuItem) => {
+        //     menuData.push(menuItem);
+        // });
+        // setMenu(menuData);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    // remove navbar from view
+    useEffect(() => {
+        if (about) {
+            const navbar = document.getElementById("navbar");
+            navbar.remove();
+        }
+    }, []);
 
     return (
         <>
             <UserNav />
             <div className="centertext">
-            <Card style={{ width: "90%" }}>
-                <Card.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                </Card.Body>
-            </Card>
-            <br></br>
-            <div style={{ width: "90% " }}>
-                <Card.Title>Connect with us on Social Media!</Card.Title>
-                <div>
-                    {" "}
-                    <a href="#">Card Link</a> <br></br>
-                    <a href="#">Card Link</a>
+                <Card style={{ width: "90%" }}>
+                    <Card.Body>{about}</Card.Body>
+                </Card>
+                <br></br>
+                <div style={{ width: "90% " }}>
+                    <Card.Title>Connect with us on Social Media!</Card.Title>
+                    <div>
+                        {" "}
+                        <a href="#">Card Link</a> <br></br>
+                        <a href="#">Card Link</a>
+                    </div>
                 </div>
-            </div>
             </div>
         </>
     );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,11 @@ export function validatePhone(phone) {
     var re = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
     return re.test(phone);
 }
+
+export function validateExists(variable) {
+    if (variable) {
+        return true;
+    } else {
+        return false;
+    }
+}
